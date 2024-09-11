a7fe734b11379901807f3b113cb6817d0c173b4b
Matias Pautasso <matiaspautasso@outlook.com>
2024-09-10 22:26:15 -0300

    cambios 22:26

--- AgendaPautasso/Form1.Designer.cs
+++ AgendaPautasso/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AgendaPautasso
 {
-    partial class Form1
+    partial class frmAgenda
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,120 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DgvAgenda = new System.Windows.Forms.DataGridView();
             this.btnAgregar = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.textNombre = new System.Windows.Forms.TextBox();
-            this.textApellido = new System.Windows.Forms.TextBox();
-            this.txtDni = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.cmbBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvAgenda)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // DgvAgenda
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(40, 171);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(702, 168);
-            this.dataGridView1.TabIndex = 0;
+            this.DgvAgenda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvAgenda.Location = new System.Drawing.Point(40, 181);
+            this.DgvAgenda.Name = "DgvAgenda";
+            this.DgvAgenda.Size = new System.Drawing.Size(702, 221);
+            this.DgvAgenda.TabIndex = 0;
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(51, 397);
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(113, 421);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(141, 50);
+            this.btnAgregar.Size = new System.Drawing.Size(141, 112);
             this.btnAgregar.TabIndex = 1;
-            this.btnAgregar.Text = "button1";
+            this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(330, 397);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(247, 421);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(141, 50);
+            this.button2.Size = new System.Drawing.Size(141, 112);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "Eliminar";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(570, 397);
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(379, 421);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(141, 50);
+            this.button3.Size = new System.Drawing.Size(141, 112);
             this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
+            this.button3.Text = "Exportar";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // textNombre
+            // button1
             // 
-            this.textNombre.Location = new System.Drawing.Point(53, 61);
-            this.textNombre.Name = "textNombre";
-            this.textNombre.Size = new System.Drawing.Size(100, 20);
-            this.textNombre.TabIndex = 4;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(516, 421);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 112);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Editar";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // textApellido
+            // cmbCategoria
             // 
-            this.textApellido.Location = new System.Drawing.Point(321, 61);
-            this.textApellido.Name = "textApellido";
-            this.textApellido.Size = new System.Drawing.Size(100, 20);
-            this.textApellido.TabIndex = 5;
-            // 
-            // txtDni
-            // 
-            this.txtDni.Location = new System.Drawing.Point(581, 61);
-            this.txtDni.Name = "txtDni";
-            this.txtDni.Size = new System.Drawing.Size(100, 20);
-            this.txtDni.TabIndex = 6;
+            this.cmbCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Location = new System.Drawing.Point(146, 56);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(201, 32);
+            this.cmbCategoria.TabIndex = 11;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 59);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "nombre";
+            this.label1.Size = new System.Drawing.Size(87, 24);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "categoria";
+            // 
+            // cmbBuscar
+            // 
+            this.cmbBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbBuscar.Location = new System.Drawing.Point(451, 59);
+            this.cmbBuscar.Name = "cmbBuscar";
+            this.cmbBuscar.Size = new System.Drawing.Size(245, 29);
+            this.cmbBuscar.TabIndex = 13;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(503, 118);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(193, 43);
+            this.btnBuscar.TabIndex = 14;
+            this.btnBuscar.Text = "BUSCAR";
+            this.btnBuscar.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(261, 32);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(447, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(43, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "apellido";
+            this.label2.Size = new System.Drawing.Size(189, 20);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "buscar por nom/tel/correo";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(522, 32);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(21, 13);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "dni";
-            // 
-            // Form1
+            // frmAgenda
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 564);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.cmbBuscar);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtDni);
-            this.Controls.Add(this.textApellido);
-            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.cmbCategoria);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.btnAgregar);
-            this.Controls.Add(this.dataGridView1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Controls.Add(this.DgvAgenda);
+            this.Name = "frmAgenda";
+            this.Text = "Agenda";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgvAgenda)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -149,16 +162,16 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView DgvAgenda;
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textNombre;
-        private System.Windows.Forms.TextBox textApellido;
-        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbCategoria;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox cmbBuscar;
+        private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
     }
 }
 
--- AgendaPautasso/Form1.cs
+++ AgendaPautasso/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace AgendaPautasso
 {
-    public partial class Form1 : Form
+    public partial class frmAgenda : Form
     {
-        public Form1()
+        public frmAgenda()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
